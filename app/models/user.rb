d376cb2130857c5694c0e03c20fd1332a83d3cfe
Rattlehead90 <strogonov@seznam.cz>
2022-11-11 21:58:22 +0100

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  before_save { email.downcase! }
+  validates :username, presence: true, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, length: { maximum: 255 },
+            format: VALID_EMAIL_REGEX
+  VALID_PASSWORD_REGEX = /\d/
+  validates :password, presence: true, length: { minimum: 6 },
+            format: VALID_PASSWORD_REGEX
 end
