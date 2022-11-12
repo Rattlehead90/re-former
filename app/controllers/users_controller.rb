b9f856a5fb201863cbdba16a7a40e7db53c95548
Rattlehead90 <strogonov@seznam.cz>
2022-11-12 11:41:27 +0100

    Add Errors warning helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
 
     if @user.update(user_params)
-      redirect_to @user
+      redirect_to edit_user_path
     else
       render :edit, status: :unprocessable_entity
     end
